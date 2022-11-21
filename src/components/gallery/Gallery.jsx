5ce2dcf1799c68f8797477e3526f25f7f6d90651
Catevika <dominique.bello@outlook.fr>
2022-11-21 11:07:47 +0100

    Add Gallery + page to Client + LazyLoadImage

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,37 +1,81 @@
 import { useState, useEffect } from 'react';
-import { FiMaximize } from 'react-icons/fi';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { searchPhotos } from '../../photo-client/Client';
-
+import { FiMaximize } from 'react-icons/fi';
+import { MdClose } from 'react-icons/md';
+import { GrCaretPrevious, GrCaretNext } from 'react-icons/gr';
 import './gallery.css';
 
 export default function Gallery({ term }) {
 	const [searching, setSearching] = useState(false);
 	const [error, setError] = useState(false);
+	const [pageNumber, setPageNumber] = useState(1);
 	const [gallery, setGallery] = useState([]);
+	const [nbPages, setNbPages] = useState(null);
+
+	const [isShowing, setIsShowing] = useState(false);
+
+	const toggle = () => setIsShowing(!isShowing);
 
 	useEffect(() => {
 		const getPhotos = async () => {
-			setSearching(true);
-			setError(false);
-			const data = await searchPhotos({ query: term, per_page: 15 });
-			setGallery(data.photos);
-			setSearching(false);
+			try {
+				setSearching(true);
+				setError(false);
+				const data = await searchPhotos({
+					query: term,
+					page: pageNumber,
+					per_page: 15
+				});
+				setNbPages(Math.ceil(data.total_results / data.per_page));
+				setGallery(data.photos);
+				setSearching(false);
+			} catch (err) {
+				setError({ error: err.message });
+				console.log(error);
+			}
 		};
 		if (term) getPhotos();
-	}, [term]);
-
-	console.log(gallery.length);
+	}, [term, pageNumber]);
 
 	return (
 		<article className='gallery__container'>
 			{!searching && !error && gallery?.length > 0 && (
+				<div className='gallery__page'>
+					<div className='gallery__buttons'>
+						<button
+							type='button'
+							className='gallery__button'
+							onClick={() => setPageNumber((pageNumber) => pageNumber - 1)}
+							disabled={pageNumber === 1}
+						>
+							<GrCaretPrevious className='gallery__icon' />
+						</button>
+						<button
+							type='button'
+							className='gallery__button'
+							onClick={() => setPageNumber((pageNumber) => pageNumber + 1)}
+							disabled={pageNumber === nbPages}
+						>
+							<GrCaretNext className='gallery__icon' />{' '}
+						</button>
+					</div>
+					<p className='gallery__page-text'>
+						{' '}
+						page: <span>{pageNumber}</span> / {nbPages}
+					</p>
+				</div>
+			)}
+
+			{!searching && !error && gallery?.length > 0 && (
 				<ul className='gallery-wrapper'>
-					{gallery.map((photo, index) => {
+					{gallery.map((photo) => {
 						return (
-							<li key={index} className='gallery__photo'>
-								<img
-									src={photo.src.medium}
-									alt={photo.url}
+							<li key={photo.id} className='gallery__photo'>
+								<LazyLoadImage
+									src={isShowing ? photo.src.large : photo.src.medium}
+									alt={photo.alt}
+									loading='lazy'
 									className='gallery__photo-img'
 								/>
 								<div className='gallery__photo-overlay'>
@@ -40,18 +84,27 @@ export default function Gallery({ term }) {
 											href={photo.photographer_url}
 											className='gallery__photo-link'
 										>
-											Photo by {photo.photographer}
+											{photo.photographer}
 										</a>
-										<FiMaximize className='gallery__photo-icon' />
+										{isShowing ? (
+											<MdClose
+												title='View medium size'
+												className='gallery__photo-icon'
+												onClick={toggle}
+											/>
+										) : (
+											<FiMaximize
+												title='View large size'
+												className='gallery__photo-icon'
+												onClick={toggle}
+											/>
+										)}
 									</p>
 								</div>
 							</li>
 						);
 					})}
 				</ul>
-			)}
-			{!searching && !error && gallery?.length > 0 && (
-				<button type='button'>Next page</button>
 			)}
 		</article>
 	);
--- src/photo-client/Client.jsx
+++ src/photo-client/Client.jsx
@@ -3,5 +3,5 @@ import { createClient } from 'pexels';
 const pexelAuthorization = import.meta.env.VITE_PEXELS_API_KEY;
 const client = createClient(pexelAuthorization);
 
-export const searchPhotos = async ({ query, per_page }) =>
-	client.photos.search({ query, per_page });
+export const searchPhotos = async ({ query, page, per_page }) =>
+	client.photos.search({ query, page, per_page });
