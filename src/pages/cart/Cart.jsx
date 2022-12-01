8786ae3106e4a3bd5a7e018ae73cce96e41e832b
Catevika <dominique.bello@outlook.fr>
2022-12-01 14:42:35 +0100

    Add the ZIP download with JSZip + Update packages

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,14 +1,49 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import JSZip from "jszip";
+import saveAs from 'file-saver';
 import { AuthContext } from '../../context/authContext/AuthContext';
 import { CartContext } from '../../context/cartContext/cartContext';
 import { HiMinusCircle } from 'react-icons/hi';
 import './cart.css';
 
-export default function Cart()
-{
+export default function Cart() {
 	const { user } = useContext(AuthContext).user;
 	const { cart, dispatch } = useContext(CartContext);
+
+	const handleDownload = () => {
+
+		const urls = cart.map(item => item.image);
+		const texts = cart.map(item => item.authorUrl);
+
+		const saveZip = (filename, urls, texts) => {
+			if (!urls) return;
+			if (!texts) return;
+
+			const zip = new JSZip();
+			const folder = zip.folder('ShopLineArt');
+			folder.file("THANK-YOU.text", " Hello, I am Catevika, the developper of the ShopLineArt app. Hope you enjoy it! I am really grateful to PEXELS that allow me to use their services for learning purpose. You can visit their original site anytime: https://www.pexels.com. See also the 'Attributions' folder. It is not mandatory but artists always appreciate it.");
+
+			urls.forEach((url) => {
+				const blobPromise = fetch(url).then((r) => {
+					if (r.status === 200) return Promise.resolve(r.blob());
+					return Promise.reject(new Error(r.statusText));
+				});
+				const name = url.substring(url.lastIndexOf("/") + 1);
+
+				folder.file(`pexels-photos/${name}`, blobPromise);
+
+				texts.forEach((text) => {
+					folder.file(`Attributions/${name}.txt`, 'Free for personal or commercial use. See the number attached to this file to attribute this photo to its autor:' + text + ' OR consider making a donation via PayPal when available on the profile.');
+				});
+			});
+
+			zip.generateAsync({ type: "blob" }).then((blob) => saveAs(blob, filename));
+		};
+
+		saveZip("ShopLineArt.zip", urls, texts);
+	};
+
 
 	return (
 		<div className='cart__container'>
@@ -27,8 +62,7 @@ export default function Cart()
 					</Link>
 				</div>
 				{cart.length > 0 ? <ul className='cart__items-list'>
-					{cart?.map((item) =>
-					{
+					{cart?.map((item) => {
 						return (
 							<li key={item.id} className='cart__item'>
 								<img
@@ -49,12 +83,11 @@ export default function Cart()
 										<p title='Description'><strong>Description:</strong> {item.description}</p>
 										<p title='Width'><strong>Width:</strong> {item.width} px - <strong>Height:</strong> {item.height} px</p>
 									</div>
-									<p title={item.authorUrl}><strong>License:</strong> Free with <strong>attribution</strong> to <a href={item.authorUrl}><span>{item.author}</span></a> </p>
-									<p title='Paypal Donation'>OR <strong>donation</strong> via <strong>PayPal</strong> if available</p>
 								</div>
 							</li>
 						);
 					})}
+					{cart.length > 0 ? <button type='button' onClick={handleDownload} className='cart__zip-btn' >Download ZIP</button> : null}
 				</ul> : null}
 			</div>
 		</div>
